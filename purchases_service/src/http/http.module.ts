8963e5bf89cffe445b7f34a85861b7558cc1b7a7
Guilherme Freire <guilherme.affreire@hotmail.com>
2022-08-25 19:10:53 -0300

    update graphql service

--- purchases_service/src/http/http.module.ts
+++ purchases_service/src/http/http.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TestController } from 'src/test/test.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import path from 'path';
+import { DatabaseModule } from '../database/database.module';
+import { TestResolver } from 'src/http/test.resolver';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [TestController],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  providers: [TestResolver],
 })
 export class HttpModule {}
