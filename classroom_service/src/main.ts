761a6edbe7fb2e6f322959df8805f41f4976f965
guilherme_freire <guilherme.freire@lionx.com.br>
2022-10-11 19:13:15 -0300

    feat: add kafka service and created new topic in kafka

--- classroom_service/src/main.ts
+++ classroom_service/src/main.ts
@@ -1,21 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import chalk from 'chalk';
 import figlet from 'figlet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'classroom',
+        brokers: ['localhost:29092'],
+      },
+    },
+  });
+
   await app.listen(4003, () =>
     console.log(
       chalk.blueBright(
-        figlet.textSync('CLASSROOM', {
+        figlet.textSync('CLASSROOM \n \n 4003', {
           horizontalLayout: 'default',
           verticalLayout: 'default',
-          width: 60,
+          width: 100,
           whitespaceBreak: true,
         }),
       ),
     ),
   );
 }
+
 bootstrap();
