54175a9205c2867f1ef1a47fe7592b3f843e53bb
guilherme_freire <guilherme.freire@lionx.com.br>
2022-10-24 16:26:32 -0300

    feat: add gateway

---
+++ gateway/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import chalk from 'chalk';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.listen(4000).then(() => {
+    console.log(chalk.blue.bold('[ Federation ] Server running in port 4003'));
+  });
+}
+
+bootstrap();
