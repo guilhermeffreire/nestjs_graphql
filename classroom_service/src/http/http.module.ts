9d2426f5ce4fd7d53f349b74714bd5cf345b5b9c
guilherme_freire <guilherme.freire@lionx.com.br>
2022-10-24 16:10:02 -0300

    feat: add students resolver

--- classroom_service/src/http/http.module.ts
+++ classroom_service/src/http/http.module.ts
@@ -3,7 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import path from 'path';
 import { DatabaseModule } from '../database/database.module';
-import { ApolloDriver } from '@nestjs/apollo';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 import { CoursesResolver } from './graphql/resolvers/courses.resolver';
 import { StudentsResolver } from './graphql/resolvers/students.resolver';
 import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
@@ -15,8 +18,8 @@ import { EnrollmentsService } from '../services/enrollments/enrollments.service'
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
-    GraphQLModule.forRoot({
-      driver: ApolloDriver,
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
