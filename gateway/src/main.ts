1d83775a59ec3a982272098e0636402fb9f06623
guilherme_freire <guilherme.freire@lionx.com.br>
2022-10-25 10:01:18 -0300

    fix: updated information console.log in all servers

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,12 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import chalk from 'chalk';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import chalk from "chalk";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.listen(4000).then(() => {
-    console.log(chalk.blue.bold('[ Federation ] Server running in port 4003'));
+    console.log(chalk.blue.bold("[ Federation ] Server running in port 4000"));
   });
 }
 
--- purchases_service/src/main.ts
+++ purchases_service/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import chalk from 'chalk';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4002);
+
+  await app.listen(4002).then(() => {
+    console.log(chalk.blue.bold('[ Purchases ] Server running in port 4002'));
+  });
 }
 bootstrap();
